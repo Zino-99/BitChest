2c571d6ee293dcf317273e5cead4f8aabf739b7f
djamel-ouazib <djamel.ouazibb@gmail.com>
2026-01-31 14:54:48 +0100

    feat: ajout des nouvelles routes avec React Router

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 function Dashboard() {
-    return <h1 className="text-4xl text-orange-500"> Dashboard</h1>
+    return <h1 className="text-4xl text-orange-700"> Dashboard</h1>
 }
 
 export default Dashboard
