dd74d4ab75cd31eb059a01f5474bc6b34c363709
djamel-ouazib <djamel.ouazibb@gmail.com>
2026-01-31 15:00:02 +0100

    Add new routes for the project

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,15 @@
 function Dashboard() {
-    return <h1 className="text-4xl text-orange-700"> Dashboard</h1>
+    return (
+        <div>
+            <h1 className="text-4xl text-orange-700"> Dashboard</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus culpa nemo quo veritatis voluptatum laboriosam
+                vel iste tenetur expedita hic quaerat repellendus adipisci
+                explicabo, aut dolorum ipsam asperiores dicta! Voluptates.
+            </p>
+        </div>
+    )
 }
 
 export default Dashboard
