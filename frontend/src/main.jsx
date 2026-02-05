2f0ee59ccab0999c746e0904723e8348ddb06708
DJADEL <djadel.vfc@gmail.com>
2026-02-05 13:19:20 +0100

    login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard.jsx'
+import Register from './pages/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />}></Route>
-                <Route path="/Dashboard" element={<Dashboard />}></Route>
+                
+                <Route path="/" element={<Register />}></Route>
             </Routes>
         </BrowserRouter>
     </StrictMode>
