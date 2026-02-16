c2d6905dd4598bace41665044b0eaae9bb8d0a7c
DJADEL <djadel.vfc@gmail.com>
2026-02-16 16:55:17 +0100

    Definition de l'architecture (composant, pages)

---
+++ frontend/src/pages/Admin/DataAdmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DataAdmin = () => {
+    return (
+        <div>
+            <h1>My Data Amin</h1>
+        </div>
+    );
+};
+
+export default DataAdmin;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@
 const Login = () => {
     return (
         <div>
-            
+            <h1>Login user</h1>
         </div>
     );
 };
