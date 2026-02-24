e94073f783c77f96c587f0ebe2306ed33c5d1f5f
djamel-ouazib <djamel.ouazibb@gmail.com>
2026-02-24 17:37:39 +0100

    Add Cryptocurrency entity

---
+++ backend/src/Entity/Cryptocurrency.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CryptocurrencyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CryptocurrencyRepository::class)]
+#[ORM\Table(name: 'cryptocurrency')]
+class Cryptocurrency
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 10, unique: true)]
+    private ?string $symbol = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    // ----------------- Getters / Setters -----------------
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSymbol(): ?string
+    {
+        return $this->symbol;
+    }
+
+    public function setSymbol(string $symbol): static
+    {
+        $this->symbol = $symbol;
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
---
+++ backend/src/Repository/CryptocurrencyRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Cryptocurrency;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class CryptocurrencyRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Cryptocurrency::class);
+    }
+}
