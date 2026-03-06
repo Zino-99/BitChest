cec7c6bb382b01824ccdfd712c8173afbaba5bdf
djamel-ouazib <djamel.ouazibb@gmail.com>
2026-03-06 14:12:10 +0100

    final version

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,3 @@
-// import { useEffect } from 'react'
-// import { useNavigate } from 'react-router-dom'
-
-// export default function Dashboard() {
-//     const navigate = useNavigate()
-//     const user = JSON.parse(sessionStorage.getItem('user'))
-
-//     useEffect(() => {
-//         if (!user) {
-//             navigate('/Login')
-//         } else if (user.role === 'admin') {
-//             navigate('/admin/Market')
-//         } else {
-//             navigate('/user/Wallet')
-//         }
-//     }, [])
-
-//     return null
-// }
-
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
